d96059a582507c65f2ec5df6fcf3892bf4d5520d
Byron <jamesblocker81@gmail.com>
2018-02-05 09:42:43 -0800

    build deck function completed and tested

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-alert(`working`);
+function card(value, name, suit) {
+    this.value = value;
+    this.name = name;
+    this.suit = suit;
+}
+
+function deck() {
+    this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+    var cards = [];
+
+    for (var i = 0; i < this.suits.length; i++) {
+        for (var j = 0; j < this.names.length; j++) {
+            cards.push(new card(j + 1, this.names[j], this.suits[i]));
+        }
+    }
+
+    return cards;
+}
+
+var deck1 = deck();
+//console.log(deck1[0]);
+
+for (var i = 0; i < deck1.length; i++) {
+    console.log(deck1[i]);
+}

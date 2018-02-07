19251c4f5cd3ef1613db14e6121f183f1696afad
Byron <jamesblocker81@gmail.com>
2018-02-07 12:07:51 -0800

    tried to add custom alert boxes but commented out instead

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ function addCardToPlayer() {
         $('#player1Hand').html(handImages);
         var newScore = getScore(player1);
         if (newScore > 21) {
-            alert('Player 1 BUST');
+            alert('Johnny BUSTS');
             player1Turn = 0;
             player2Turn = 1;
         }
@@ -154,7 +154,7 @@ function addCardToPlayer() {
         $('#player2Hand').html(handImages);
         var newScore = getScore(player2);
         if (newScore > 21) {
-            alert('Player 2 BUST');
+            alert('Sid BUSTS');
             player2Turn = 0;
             dealerTurn();
         }
@@ -304,3 +304,50 @@ function updateWins(player) {
         $('#wins2').text(player2.wins);
     }
 }
+
+// alert boxes
+// var ALERT_TITLE = "Oops!";
+// var ALERT_BUTTON_TEXT = "Ok";
+
+// if(document.getElementById) {
+//     window.alert = function(txt) {
+//         createCustomAlert(txt);
+//     }
+// }
+
+// function createCustomAlert(txt, title) {
+//     d = document;
+
+//     if(d.getElementById("modalContainer")) return;
+
+//     mObj = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
+//     mObj.id = "modalContainer";
+//     mObj.style.height = d.documentElement.scrollHeight + "px";
+
+//     alertObj = mObj.appendChild(d.createElement("div"));
+//     alertObj.id = "alertBox";
+//     if(d.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+//     alertObj.style.left = (d.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+//     alertObj.style.visiblity="visible";
+
+//     h1 = alertObj.appendChild(d.createElement("h1"));
+//     h1.appendChild(d.createTextNode(title));
+
+//     msg = alertObj.appendChild(d.createElement("p"));
+//     //msg.appendChild(d.createTextNode(txt));
+//     msg.innerHTML = txt;
+
+//     btn = alertObj.appendChild(d.createElement("a"));
+//     btn.id = "closeBtn";
+//     btn.appendChild(d.createTextNode(ALERT_BUTTON_TEXT));
+//     btn.href = "#";
+//     btn.focus();
+//     btn.onclick = function() { removeCustomAlert();return false; }
+
+//     alertObj.style.display = "block";
+
+// }
+
+// function removeCustomAlert() {
+//     document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
+// }
